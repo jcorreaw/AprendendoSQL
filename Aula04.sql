9f2eb12bf745b0da95e2b258d0092bfc0fe53147
jcorreaw <juninhocorrea2014@gmail.com>
2023-09-25 10:40:07 -0300

    aula04 e 05

---
+++ Aula04.sql
@@ -0,0 +1,14 @@
+ALTER TABLE Usuarios
+ADD rua VARCHAR(100),
+ADD numero VARCHAR(10),
+ADD cidade VARCHAR(50),
+ADD estado VARCHAR(50);
+
+UPDATE usuarios
+SET rua = SUBSTRING_INDEX(SUBSTRING_INDEX(endereco, ',', 1), ',', -1),
+    numero = SUBSTRING_INDEX(SUBSTRING_INDEX(endereco, ',', 2), ',', -1),
+    cidade = SUBSTRING_INDEX(SUBSTRING_INDEX(endereco, ',', 3), ',', -1),
+    estado = SUBSTRING_INDEX(endereco, ',', -1);
+
+    ALTER TABLE usuarios
+DROP COLUMN endereco;
